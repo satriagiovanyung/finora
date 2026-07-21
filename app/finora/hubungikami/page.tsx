97cb65bf1d76ea141ad597155459faefb588e99e
SATRIA GIOVAN <satriagiovan505@gmail.com>
2026-07-21 13:09:31 +0700

    Fix error tipe icon di hubungi kami

--- app/finora/hubungikami/page.tsx
+++ app/finora/hubungikami/page.tsx
@@ -107,7 +107,10 @@ export default function HubungiKamiPage() {
 
                     <div className="mb-2">
                       <div className="flex items-center gap-2 mb-1">
-                        {item.type === 'lucide' && <item.icon className="w-4 h-4 text-white" />}
+                        {item.type === 'lucide' && (() => {
+  const Icon: any = item.icon;
+  return <Icon className="w-4 h-4 text-white" />;
+})()}
                         {item.type === 'insta' && (
                           <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
                         )}
