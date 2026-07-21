8ec1bc61f98d56fafa9bc4f94ab986f3bebd5a1c
SATRIA GIOVAN <satriagiovan505@gmail.com>
2026-07-21 12:15:29 +0700

    Memperbaiki tipe ease framer motion

--- app/finora/hubungikami/page.tsx
+++ app/finora/hubungikami/page.tsx
@@ -11,7 +11,7 @@ const fadeUpVariant = {
   visible: { 
     opacity: 1, 
     y: 0, 
-    transition: { duration: 0.6, ease: "easeOut" } 
+    transition: { duration: 0.6, ease: "easeOut" as const} 
   },
 };
 
